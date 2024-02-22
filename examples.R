6389298ba4adadb5fae1ed38cd181a6a778d2993
Danielle Navarro <djnavarro@protonmail.com>
2024-02-23 01:08:14 +1100

    tweak example

--- examples.R
+++ examples.R
@@ -2,9 +2,10 @@ source(here::here("sketches.R"))
 
 dat <- tibble::tibble(
   fill = c("#e50000", "#ff8d00", "#ffee00", "#028121", "#004cff", "#770088"),
-  linewidth = 0,
+  color = fill,
   x_center = cos(seq(0, pi * 5/3, length.out = 6)),
-  y_center = sin(seq(0, pi * 5/3, length.out = 6))
+  y_center = sin(seq(0, pi * 5/3, length.out = 6)),
+  n = 500
 )
 
 sketch(shapes = purrr::pmap(dat, blob)) |> draw()
